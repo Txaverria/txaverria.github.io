5929ccb8e13124d9fde8462d770c8f59344903f4
Txaverria <isaac.chc24@gmail.com>
2024-05-24 18:10:40 -0600

    General updates and fixes

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,112 +1,110 @@
-const container = document.getElementById('container-console');
-const closeButton = document.getElementById('closeButton');
-const maximizeButton = document.getElementById('maximizeButton');
-const hideButton = document.getElementById('hideButton');
-const taskbarStart = document.getElementById('taskbarStart');
-const taskbarItem = document.getElementById('taskbarItem');
-const taskbarItemImg = taskbarItem.querySelector('img');
+const container = document.getElementById("container-console");
+const closeButton = document.getElementById("closeButton");
+const maximizeButton = document.getElementById("maximizeButton");
+const hideButton = document.getElementById("hideButton");
+const taskbarStart = document.getElementById("taskbarStart");
+const taskbarItem = document.getElementById("taskbarItem");
+const taskbarItemImg = taskbarItem.querySelector("img");
 
 function copyText(text) {
-	var textArea = document.createElement('textarea');
-	textArea.value = text;
-	document.body.appendChild(textArea);
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+    document.body.appendChild(textArea);
 
-	textArea.select();
-	document.execCommand('copy');
-	document.body.removeChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
 
-	Swal.fire({
-		position: 'bottom-start',
-		icon: 'success',
-		title: '<span style="font-size: 1.2rem;">Copied to clipboard</span>',
-		toast: true,
-		showConfirmButton: false,
-		timer: 1750,
-		timerProgressBar: true,
-	});
+    Swal.fire({
+        position: "bottom-start",
+        icon: "success",
+        title: '<span style="font-size: 1.2rem;">Copied to clipboard</span>',
+        toast: true,
+        showConfirmButton: false,
+        timer: 1750,
+        timerProgressBar: true,
+    });
 }
 
 let maximizeImageIndex = 1;
 
 let currentPosition = 0;
 
-closeButton.addEventListener('click', () => {
-	container.style.animation = 'shake 0.5s ease';
-	setTimeout(() => {
-		container.style.animation = '';
-	}, 500);
+closeButton.addEventListener("click", () => {
+    container.style.animation = "shake 0.5s ease";
+    setTimeout(() => {
+        container.style.animation = "";
+    }, 500);
 });
 
-maximizeButton.addEventListener('click', () => {
-	container.classList.toggle('width75');
-	container.classList.toggle('width100');
+maximizeButton.addEventListener("click", () => {
+    container.classList.toggle("width75");
+    container.classList.toggle("width100");
 
-	if (maximizeImageIndex === 1) {
-		maximizeButton.src =
-			'img/portfolio/terminal/minimize-window.webp';
-		console.log(maximizeImageIndex);
-		maximizeImageIndex = 2;
-	} else {
-		maximizeButton.src =
-			'img/portfolio/terminal/maximize-button.webp';
-		console.log(maximizeImageIndex);
-		maximizeImageIndex = 1;
-	}
+    if (maximizeImageIndex === 1) {
+        maximizeButton.src = "img/portfolio/terminal/minimize-window.webp";
+        console.log(maximizeImageIndex);
+        maximizeImageIndex = 2;
+    } else {
+        maximizeButton.src = "img/portfolio/terminal/maximize-button.webp";
+        console.log(maximizeImageIndex);
+        maximizeImageIndex = 1;
+    }
 });
 
-hideButton.addEventListener('click', hideWindow);
+hideButton.addEventListener("click", hideWindow);
 
 function hideWindow() {
-	container.classList.add('opacity-0');
-	currentPosition += 50;
-	container.style.transform = `translateY(${currentPosition}px)`;
-	taskbarItem.style.color = 'black';
-	taskbarItem.style.background = 'rgb(222, 226, 230)';
-	taskbarItemImg.style.filter = 'invert(0)';
-	taskbarItemImg.src = 'img/portfolio/terminal/sort-up.webp';
-	taskbarItem.style.cursor = 'pointer';
+    container.classList.add("opacity-0");
+    currentPosition += 50;
+    container.style.transform = `translateY(${currentPosition}px)`;
+    taskbarItem.style.color = "black";
+    taskbarItem.style.background = "rgb(222, 226, 230)";
+    taskbarItemImg.style.filter = "invert(0)";
+    taskbarItemImg.src = "img/portfolio/terminal/sort-up.webp";
+    taskbarItem.style.cursor = "pointer";
 
-	// Disable the second event listener
-	taskbarItem.addEventListener('click', showWindow);
+    // Disable the second event listener
+    taskbarItem.addEventListener("click", showWindow);
 }
 
 function showWindow() {
-	taskbarItem.style.color = 'white';
-	taskbarItemImg.style.filter = 'invert(1)';
-	currentPosition -= 50;
-	container.style.transform = `translateY(${currentPosition}px)`;
-	taskbarItem.style.background = 'none';
-	container.classList.remove('opacity-0');
-	taskbarItemImg.src = 'img/portfolio/terminal/sort-down.webp';
-	taskbarItem.style.cursor = 'default';
+    taskbarItem.style.color = "white";
+    taskbarItemImg.style.filter = "invert(1)";
+    currentPosition -= 50;
+    container.style.transform = `translateY(${currentPosition}px)`;
+    taskbarItem.style.background = "none";
+    container.classList.remove("opacity-0");
+    taskbarItemImg.src = "img/portfolio/terminal/sort-down.webp";
+    taskbarItem.style.cursor = "default";
 
-	// Disable the second event listener
-	taskbarItem.removeEventListener('click', showWindow);
+    // Disable the second event listener
+    taskbarItem.removeEventListener("click", showWindow);
 }
 
-taskbarStart.addEventListener('click', () => {
-	window.location.href = '#about-me';
+taskbarStart.addEventListener("click", () => {
+    window.location.href = "#about-me";
 });
 
 function updateTime() {
-	const clockElement = document.getElementById('clock');
-	const currentTime = new Date();
-	let hours = currentTime.getHours();
-	const minutes = currentTime.getMinutes().toString().padStart(2, '0');
+    const clockElement = document.getElementById("clock");
+    const currentTime = new Date();
+    let hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes().toString().padStart(2, "0");
 
-	let amPm = 'AM';
+    let amPm = "AM";
 
-	if (hours > 12) {
-		hours -= 12;
-		amPm = 'PM';
-	}
+    if (hours > 12) {
+        hours -= 12;
+        amPm = "PM";
+    }
 
-	hours = hours.toString().padStart(2, '');
+    hours = hours.toString().padStart(2, "");
 
-	const timeString = `${hours}:${minutes} ${amPm}`;
-	clockElement.textContent = timeString;
+    const timeString = `${hours}:${minutes} ${amPm}`;
+    clockElement.textContent = timeString;
 }
 
 // Update the time immediately and then every second
 updateTime();
-setInterval(updateTime, 10000);
+setInterval(updateTime, 2000);
